84bff3d6412e4d6ec5b6070c39546cd388bd9df0
Jash ashar <jash.ashar@deptagency.com>
2025-04-04 16:57:56 +0530

    sidebar fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Dashboard from "./components/Dashboard";
-import Sidebar from "./components/sidebar";
+import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import data from './data/data.json';
 
